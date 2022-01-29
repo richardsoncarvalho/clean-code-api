eb0367233076acf8d16ddbf10e7c819419a3cbe4
Richardson Bruno <richardsonbrc.ads@gmail.com>
2022-01-29 09:30:39 -0300

    refactor: to duplicated code to a helper code

--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -1,15 +1,13 @@
 import { HttpResponse, HttpRequest } from '../protocols/http'
-import { MissingParamsError } from '../errors/MissingParamsError'
+import { MissingParamsError } from '../errors/MissingParamError'
+import { badRequest } from '../helpers/http-request'
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
-        return {
-          statusCode: 400,
-          body: new MissingParamsError(field)
-        }
+        return badRequest(new MissingParamsError(field))
       }
     }
 
